b641423cab61bfacb9c92f36d2e12ba88b0aa488
Serhii <malgothsm@gmail.com>
2021-09-04 18:29:43 +0300

    Java test file

---
+++ src/testJava1/forTesting.java
@@ -0,0 +1,35 @@
+package testJava1;
+
+public class forTesting {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+//		System.out.println("Printing something");
+//		System.out.println("Something text 1");
+//		System.out.println("Print me");
+//		int result = 10;
+//		result = result * 15;
+//		++result;
+//		if (result >= 4 &&  result != 10) {
+//			System.out.println("Kinda True");
+//		} else {
+//			System.out.println(result);
+//		}
+		
+		//// Побітові операції
+//		int i = 10;
+//		int n = i++;
+//		int m = ++i;
+//		System.out.println(n);
+//		System.out.println(m);
+		
+		
+		////Math
+		System.out.println(Math.abs(-50));
+		System.out.println(Math.max(4.5, 17.5));
+	
+		
+	}
+
+}
