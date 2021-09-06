c2ce93732feaa9dc734acbbb373f2a4d5209030a
Serhii <malgothsm@gmail.com>
2021-09-06 22:41:49 +0300

    Finished switch programm

--- src/testJava1/forTesting.java
+++ src/testJava1/forTesting.java
@@ -1,6 +1,5 @@
 package testJava1;
 
-import sun.security.provider.JavaKeyStore.CaseExactJKS;
 
 public class forTesting {
 
@@ -42,23 +41,28 @@ public class forTesting {
 //		System.out.println(messString);
 		
 		  //Switch Statements
-		String timeOfTheWeek = "Sunday";
+		String timeOfTheWeek = "Friday";
 			switch (timeOfTheWeek) {
 			case "Saturday" :
+				System.out.println("Today is Saturday");
 				break;
 			case "Monday":
+				System.out.println("Today is Monday");
 				break;
 			case "Tuesday":
+				System.out.println("Today is Tuesday");
 				break;
 			case "Wednesday":
+				System.out.println("Today is Wednesday");
 				break;
 			case "Thursday":
+				System.out.println("Today is Thursday");
 				break;
 			case "Friday":
+				System.out.println("Today is Friday");
 				break;
 			default:
-				break;
-			
+				System.out.println("It's Sunday today");
 			}
 		
 	}
